aeb1b0b4398d0aa5fe21cd8bea429280c52fb34a
Slim Gharbi <59753023+SlimGharbii@users.noreply.github.com>
2024-10-18 09:18:48 +0100

    Initial commit

---
+++ WarehouseFIFO/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        dgvFIFO = New DataGridView()
+        txtItemID = New TextBox()
+        txtQuantity = New TextBox()
+        txtItemName = New TextBox()
+        btnAddFIFO = New Button()
+        btnRemoveFIFO = New Button()
+        dtpDateReceived = New DateTimePicker()
+        PRF = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtBarcodeScan = New TextBox()
+        Label4 = New Label()
+        PictureBox1 = New PictureBox()
+        btnPrintPDF = New Button()
+        CType(dgvFIFO, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' dgvFIFO
+        ' 
+        dgvFIFO.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvFIFO.Location = New Point(6, 80)
+        dgvFIFO.Name = "dgvFIFO"
+        dgvFIFO.RowHeadersWidth = 51
+        dgvFIFO.Size = New Size(646, 303)
+        dgvFIFO.TabIndex = 0
+        ' 
+        ' txtItemID
+        ' 
+        txtItemID.Location = New Point(779, 123)
+        txtItemID.Name = "txtItemID"
+        txtItemID.Size = New Size(125, 27)
+        txtItemID.TabIndex = 1
+        ' 
+        ' txtQuantity
+        ' 
+        txtQuantity.Location = New Point(779, 189)
+        txtQuantity.Name = "txtQuantity"
+        txtQuantity.Size = New Size(125, 27)
+        txtQuantity.TabIndex = 3
+        ' 
+        ' txtItemName
+        ' 
+        txtItemName.Location = New Point(779, 156)
+        txtItemName.Name = "txtItemName"
+        txtItemName.Size = New Size(125, 27)
+        txtItemName.TabIndex = 4
+        ' 
+        ' btnAddFIFO
+        ' 
+        btnAddFIFO.Location = New Point(160, 474)
+        btnAddFIFO.Name = "btnAddFIFO"
+        btnAddFIFO.Size = New Size(94, 29)
+        btnAddFIFO.TabIndex = 5
+        btnAddFIFO.Text = "Ajout PRF"
+        btnAddFIFO.UseVisualStyleBackColor = True
+        ' 
+        ' btnRemoveFIFO
+        ' 
+        btnRemoveFIFO.Location = New Point(835, 474)
+        btnRemoveFIFO.Name = "btnRemoveFIFO"
+        btnRemoveFIFO.Size = New Size(94, 29)
+        btnRemoveFIFO.TabIndex = 6
+        btnRemoveFIFO.Text = "Sotrie PRF"
+        btnRemoveFIFO.UseVisualStyleBackColor = True
+        ' 
+        ' dtpDateReceived
+        ' 
+        dtpDateReceived.Location = New Point(826, 236)
+        dtpDateReceived.Name = "dtpDateReceived"
+        dtpDateReceived.Size = New Size(234, 27)
+        dtpDateReceived.TabIndex = 7
+        ' 
+        ' PRF
+        ' 
+        PRF.AutoSize = True
+        PRF.Location = New Point(661, 123)
+        PRF.Name = "PRF"
+        PRF.Size = New Size(33, 20)
+        PRF.TabIndex = 8
+        PRF.Text = "PRF"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(661, 156)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(56, 20)
+        Label1.TabIndex = 9
+        Label1.Text = "Famille"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(661, 189)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(66, 20)
+        Label2.TabIndex = 10
+        Label2.Text = "Quantité"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(658, 241)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(162, 20)
+        Label3.TabIndex = 11
+        Label3.Text = "Choisir la date d'entrée"
+        ' 
+        ' txtBarcodeScan
+        ' 
+        txtBarcodeScan.Location = New Point(160, 389)
+        txtBarcodeScan.Name = "txtBarcodeScan"
+        txtBarcodeScan.Size = New Size(602, 27)
+        txtBarcodeScan.TabIndex = 12
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 28.2F, FontStyle.Bold Or FontStyle.Underline, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = SystemColors.Highlight
+        Label4.ImageAlign = ContentAlignment.TopCenter
+        Label4.Location = New Point(255, 6)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(603, 62)
+        Label4.TabIndex = 13
+        Label4.Text = "Gestion FIFO magasin EOT"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BorderStyle = BorderStyle.Fixed3D
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(12, 6)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(237, 65)
+        PictureBox1.TabIndex = 14
+        PictureBox1.TabStop = False
+        ' 
+        ' btnPrintPDF
+        ' 
+        btnPrintPDF.Location = New Point(12, 389)
+        btnPrintPDF.Name = "btnPrintPDF"
+        btnPrintPDF.Size = New Size(94, 29)
+        btnPrintPDF.TabIndex = 15
+        btnPrintPDF.Text = "Print PDF"
+        btnPrintPDF.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1102, 515)
+        Controls.Add(btnPrintPDF)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label4)
+        Controls.Add(txtBarcodeScan)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PRF)
+        Controls.Add(dtpDateReceived)
+        Controls.Add(btnRemoveFIFO)
+        Controls.Add(btnAddFIFO)
+        Controls.Add(txtItemName)
+        Controls.Add(txtQuantity)
+        Controls.Add(txtItemID)
+        Controls.Add(dgvFIFO)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgvFIFO, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents dgvFIFO As DataGridView
+    Friend WithEvents txtItemID As TextBox
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents txtItemName As TextBox
+    Friend WithEvents btnAddFIFO As Button
+    Friend WithEvents btnRemoveFIFO As Button
+    Friend WithEvents dtpDateReceived As DateTimePicker
+    Friend WithEvents PRF As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtBarcodeScan As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnPrintPDF As Button
+
+End Class
---
+++ WarehouseFIFO/Form1.vb
@@ -0,0 +1,299 @@
+﻿Imports System.Data.OleDb
+Imports System.Text.RegularExpressions
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.IO
+Imports System.Windows.Forms
+
+
+Public Class Form1
+    Private FIFO As New List(Of FIFOItem)
+    Private connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=d:\s.gharbi\Desktop\CMFB.accdb;"
+
+    Private Class FIFOItem
+        Public Property ItemID As String
+        Public Property ItemName As String
+        Public Property Quantity As Integer
+        Public Property DateReceived As Date
+        Public Property RackAddress As String
+
+    End Class
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dgvFIFO.ColumnCount = 5
+        dgvFIFO.Columns(0).Name = "Item ID"
+        dgvFIFO.Columns(1).Name = "Item Name"
+        dgvFIFO.Columns(2).Name = "Quantity"
+        dgvFIFO.Columns(3).Name = "Date Received"
+        dgvFIFO.Columns(4).Name = "RackAdress"
+        dgvFIFO.AllowUserToAddRows = False
+    End Sub
+
+    Private Sub btnAddFIFO_Click(sender As Object, e As EventArgs) Handles btnAddFIFO.Click
+        If ValidateInputFields() Then
+            AddFIFOItem(txtItemID.Text, txtItemName.Text, Integer.Parse(txtQuantity.Text), dtpDateReceived.Value)
+        End If
+    End Sub
+
+    Private Sub btnRemoveFIFO_Click(sender As Object, e As EventArgs) Handles btnRemoveFIFO.Click
+        If ValidateRemoveFields() Then
+            Try
+                RemoveFIFO(txtItemID.Text, Integer.Parse(txtQuantity.Text))
+                UpdateDataGridView()
+                ClearInputFields()
+                MessageBox.Show("Item removed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Error removing item: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Function ValidateInputFields() As Boolean
+        If String.IsNullOrWhiteSpace(txtItemID.Text) OrElse String.IsNullOrWhiteSpace(txtItemName.Text) OrElse String.IsNullOrWhiteSpace(txtQuantity.Text) Then
+            MessageBox.Show("Please fill in all fields.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+        If Not Integer.TryParse(txtQuantity.Text, Nothing) OrElse Integer.Parse(txtQuantity.Text) <= 0 Then
+            MessageBox.Show("Quantity must be a positive integer.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Function ValidateRemoveFields() As Boolean
+        If String.IsNullOrWhiteSpace(txtItemID.Text) OrElse String.IsNullOrWhiteSpace(txtQuantity.Text) Then
+            MessageBox.Show("Please fill in all fields.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+        If Not Integer.TryParse(txtQuantity.Text, Nothing) OrElse Integer.Parse(txtQuantity.Text) <= 0 Then
+            MessageBox.Show("Quantity must be a positive integer.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Sub RemoveFIFO(itemID As String, quantityToRemove As Integer)
+        For Each item In FIFO.Where(Function(i) i.ItemID = itemID).OrderBy(Function(i) i.DateReceived).ToList()
+            If quantityToRemove <= 0 Then Exit For
+
+            If item.Quantity > quantityToRemove Then
+                item.Quantity -= quantityToRemove
+                quantityToRemove = 0
+            Else
+                quantityToRemove -= item.Quantity
+                FIFO.Remove(item)
+                MarkRackAddressAsAvailable(itemID)
+            End If
+        Next
+    End Sub
+
+    Private Sub MarkRackAddressAsAvailable(itemID As String)
+        Using connection As New OleDbConnection(connectionString)
+            Dim query As String = "UPDATE RackAddresses SET IsFull = False WHERE RackAddress = (SELECT RackAddress FROM x1 WHERE ItemID = @ItemID)"
+            Using command As New OleDbCommand(query, connection)
+                command.Parameters.AddWithValue("@ItemID", itemID)
+                connection.Open()
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+
+    Private Sub UpdateDataGridView()
+        dgvFIFO.Rows.Clear()
+        For Each item In FIFO
+            dgvFIFO.Rows.Add(item.ItemID, item.ItemName, item.Quantity, item.DateReceived, item.RackAddress)
+        Next
+    End Sub
+
+
+    Private Sub ClearInputFields()
+        txtItemID.Clear()
+        txtItemName.Clear()
+        txtQuantity.Clear()
+        txtBarcodeScan.Clear()
+        dtpDateReceived.Value = DateTime.Now
+    End Sub
+
+    Private Sub txtBarcodeScan_TextChanged(sender As Object, e As EventArgs) Handles txtBarcodeScan.TextChanged
+        Dim barcodeText As String = txtBarcodeScan.Text
+        Dim patternItemID As String = "PRF\d{8}"
+        Dim matchItemID As Match = Regex.Match(barcodeText, patternItemID)
+        Dim patternQuantity As String = "Q(\d{2})"
+        Dim matchQuantity As Match = Regex.Match(barcodeText, patternQuantity)
+
+        If matchItemID.Success AndAlso matchQuantity.Success Then
+            Dim itemID As String = matchItemID.Value
+            Dim quantity As Integer = Integer.Parse(matchQuantity.Groups(1).Value)
+            Dim itemName As String = GetItemNameFromDatabase(itemID)
+
+            If Not String.IsNullOrEmpty(itemName) Then
+                AddFIFOItem(itemID, itemName, quantity, DateTime.Now)
+                ClearInputFields()
+            End If
+        End If
+    End Sub
+
+    Private Function GetItemNameFromDatabase(itemID As String) As String
+        Dim itemName As String = String.Empty
+        Using connection As New OleDbConnection(connectionString)
+            Dim query As String = "SELECT ItemName FROM x1 WHERE ItemID = @ItemID"
+            Using command As New OleDbCommand(query, connection)
+                command.Parameters.AddWithValue("@ItemID", itemID)
+                connection.Open()
+                Dim result As Object = command.ExecuteScalar()
+                If result IsNot Nothing Then
+                    itemName = result.ToString()
+                End If
+            End Using
+        End Using
+        Return itemName
+    End Function
+
+    Private Sub AddFIFOItem(itemID As String, itemName As String, quantity As Integer, dateReceived As Date)
+        If String.IsNullOrWhiteSpace(itemID) OrElse String.IsNullOrWhiteSpace(itemName) OrElse quantity <= 0 Then
+            MessageBox.Show("Invalid item details.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim rackAddress As String = GetRandomAvailableRackAddress()
+
+        If String.IsNullOrEmpty(rackAddress) Then
+            MessageBox.Show("No available rack address found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim existingItem = FIFO.FirstOrDefault(Function(i) i.ItemID = itemID)
+        If existingItem IsNot Nothing Then
+            existingItem.Quantity += quantity
+        Else
+            Try
+                Dim item As New FIFOItem With {
+                .ItemID = itemID,
+                .ItemName = itemName,
+                .Quantity = quantity,
+                .DateReceived = dateReceived,
+                .RackAddress = rackAddress ' Assign the Rack Address
+            }
+
+                FIFO.Add(item)
+
+                ' Mark the rack address as full in the database
+                MarkRackAddressAsFull(rackAddress)
+            Catch ex As Exception
+                MessageBox.Show("Error adding item: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End Try
+        End If
+        UpdateDataGridView()
+    End Sub
+
+    Private Sub MarkRackAddressAsFull(rackAddress As String)
+        Using connection As New OleDbConnection(connectionString)
+            Dim query As String = "UPDATE RackAddresses SET IsFull = True WHERE RackAddress = @RackAddress"
+            Using command As New OleDbCommand(query, connection)
+                command.Parameters.AddWithValue("@RackAddress", rackAddress)
+                connection.Open()
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+
+
+
+    Private Sub btnPrintPDF_Click(sender As Object, e As EventArgs) Handles btnPrintPDF.Click
+        PrintPDF()
+    End Sub
+    Private Sub PrintPDF()
+        ' Create a SaveFileDialog to prompt the user for a save location
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*"
+        saveFileDialog.Title = "Save PDF File"
+        saveFileDialog.FileName = "output.pdf"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Define the PDF document
+            Dim doc As New Document()
+            Try
+                ' Create a file stream to save the PDF file
+                Dim output As New FileStream(saveFileDialog.FileName, FileMode.Create)
+                Dim writer As PdfWriter = PdfWriter.GetInstance(doc, output)
+
+                ' Open the document for writing
+                doc.Open()
+
+                ' Add a title to the document
+                Dim baseFont As BaseFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED)
+                Dim titleFont As New Font(baseFont, 18, Font.Bold)
+                Dim title As New Paragraph("Inventaire magasin", titleFont)
+                title.Alignment = Element.ALIGN_CENTER
+                doc.Add(title)
+
+                ' Add a blank line
+                doc.Add(New Paragraph(Environment.NewLine))
+
+                ' Create a table with the same number of columns as the DataGridView
+                Dim table As New PdfPTable(dgvFIFO.ColumnCount)
+
+                ' Add the headers from the DataGridView to the table
+                For Each column As DataGridViewColumn In dgvFIFO.Columns
+                    Dim cell As New PdfPCell(New Phrase(column.HeaderText))
+                    cell.BackgroundColor = BaseColor.LIGHT_GRAY
+                    table.AddCell(cell)
+                Next
+
+                ' Add the data from the DataGridView to the table
+                For Each row As DataGridViewRow In dgvFIFO.Rows
+                    For Each cell As DataGridViewCell In row.Cells
+                        table.AddCell(cell.Value?.ToString())
+                    Next
+                Next
+
+                ' Add the table to the document
+                doc.Add(table)
+
+                ' Close the document
+                doc.Close()
+                MessageBox.Show("PDF created successfully! Saved at: " & saveFileDialog.FileName, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Error creating PDF: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Finally
+                If doc.IsOpen Then doc.Close()
+            End Try
+        End If
+    End Sub
+    Private Function GetRandomAvailableRackAddress() As String
+        Dim rackAddress As String = String.Empty
+        Try
+            Using connection As New OleDbConnection(connectionString)
+                Dim query As String = "SELECT RackAddress FROM RackAddresses WHERE IsFull = False"
+                Using command As New OleDbCommand(query, connection)
+                    connection.Open()
+                    Dim reader As OleDbDataReader = command.ExecuteReader()
+                    Dim availableAddresses As New List(Of String)
+                    While reader.Read()
+                        availableAddresses.Add(reader("RackAddress").ToString())
+                    End While
+
+                    If availableAddresses.Count > 0 Then
+                        Dim random As New Random()
+                        rackAddress = availableAddresses(random.Next(availableAddresses.Count))
+                    Else
+                        MessageBox.Show("No available rack addresses found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    End If
+                End Using
+            End Using
+        Catch ex As OleDbException
+            MessageBox.Show("Database error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return rackAddress
+    End Function
+
+
+
+
+End Class
